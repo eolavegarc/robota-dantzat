6270d5cbce4a3ee6bc2dbd0cf7bf268cc5de5303
eolavegarc <93262443+eolavegarc@users.noreply.github.com>
2022-05-11 13:55:43 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 6; index++) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
-        basic.pause(500)
+    for (let index = 0; index < 4; index++) {
+        for (let index = 0; index < 6; index++) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
+            basic.pause(500)
+            DFRobotMaqueenPlus.mototStop(Motors.ALL)
+            basic.pause(100)
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 40)
+            basic.pause(500)
+            DFRobotMaqueenPlus.mototStop(Motors.ALL)
+            basic.pause(100)
+        }
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 40)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 40)
+        basic.pause(1000)
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
-        basic.pause(100)
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 40)
-        basic.pause(500)
-        DFRobotMaqueenPlus.mototStop(Motors.ALL)
-        basic.pause(100)
     }
-    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 40)
-    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 40)
-    basic.pause(1000)
-    DFRobotMaqueenPlus.mototStop(Motors.ALL)
 })
 DFRobotMaqueenPlus.I2CInit()
